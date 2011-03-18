047cb95b78ea7ea1b6c81f143abcfa8a4d674064
huangkaiguo <lennyapple@gmail.com>
2011-03-18 17:42:56 +0800

    eOrder: Change welcome interface
    new: 4. Add background alpha value
         5. use local variables instead of members
    
    1. Modify package structure of ui
    2. Add background picture
    3. Add support for landscape and multi-lauguage

--- src/com/androidodc/eorder/ui/WelcomeActivity.java
+++ src/com/androidodc/eorder/ui/WelcomeActivity.java
@@ -4,29 +4,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class WelcomeActivity extends Activity {
 
-    private TextView mWelcomeView = null;
-    private Button mEnterButton = null;
+    private final int BACKGROUND_ALPHA = 66;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.welcome);
 
-        mWelcomeView = (TextView) findViewById(R.id.welcome);
+        View welcomeLayout = findViewById(R.id.layout_welcome);
+        welcomeLayout.getBackground().setAlpha(BACKGROUND_ALPHA);
 
+        TextView welcomeView = (TextView) findViewById(R.id.welcome);
         String welcome = getString(R.string.welcome, getHotelName());
-        mWelcomeView.setText(welcome);
+        welcomeView.setText(welcome);
 
-        mEnterButton = (Button) findViewById(R.id.enter_button);
-        mEnterButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
+        Button enterButton = (Button) findViewById(R.id.enter_button);
+        enterButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
                 // TODO: The following code will be used, when TableAcitivity is created.
                 // startActivity(new Intent(WelcomeActivity.this, TablesActivity.class));
