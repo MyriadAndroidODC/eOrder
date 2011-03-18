27cd57bdeebea4975afb3b89e1b3b11a5fc7160a
huangkaiguo <lennyapple@gmail.com>
2011-03-18 18:20:41 +0800

    eOrder: Change welcome interface
    new2: 6. Remove title bar in welcome
          7. Remove some unnecessary strings
    
    new: 4. Add background alpha value
         5. use local variables instead of members
    
         1. Modify package structure of ui
         2. Add background picture
         3. Add support for landscape and multi-lauguage

--- src/com/androidodc/eorder/ui/WelcomeActivity.java
+++ src/com/androidodc/eorder/ui/WelcomeActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,15 +15,13 @@ public class WelcomeActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.welcome);
 
         View welcomeLayout = findViewById(R.id.layout_welcome);
         welcomeLayout.getBackground().setAlpha(BACKGROUND_ALPHA);
 
-        TextView welcomeView = (TextView) findViewById(R.id.welcome);
-        String welcome = getString(R.string.welcome, getHotelName());
-        welcomeView.setText(welcome);
+        TextView hotelName = (TextView) findViewById(R.id.hotel_name);
+        hotelName.setText(getHotelName());
 
         Button enterButton = (Button) findViewById(R.id.enter_button);
         enterButton.setOnClickListener(new View.OnClickListener() {
