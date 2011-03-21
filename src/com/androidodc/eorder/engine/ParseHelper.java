5864ef332f8bf885422604253a7e5a43a086c4fd
Carl Li <carlli.testman@gmail.com>
2011-03-21 14:09:23 +0800

    Add initial files about engine to CarlLi branch.

---
+++ src/com/androidodc/eorder/engine/ParseHelper.java
@@ -0,0 +1,166 @@
+package com.androidodc.eorder.engine;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.androidodc.eorder.datatypes.Category;
+import com.androidodc.eorder.datatypes.Config;
+import com.androidodc.eorder.datatypes.DiningTable;
+import com.androidodc.eorder.datatypes.Dish;
+import com.androidodc.eorder.datatypes.DishCategory;
+import com.androidodc.eorder.datatypes.Order;
+import com.androidodc.eorder.datatypes.OrderDetail;
+
+public class ParseHelper {
+    public List<Dish> parseDishsResp(String dishsRespStr) throws JSONException{
+        List<Dish> resultList = null;
+        //JSONObject jResp = Util.parseJson(responseStr);
+        JSONObject dishs = new JSONObject(dishsRespStr);
+        resultList = new ArrayList<Dish>();
+        int i;
+        Dish tempDish = null;
+        JSONArray dishArray = dishs.getJSONArray("dishs");
+        for (i = 0;i < dishArray.length();i ++){
+            tempDish = new Dish();
+            JSONObject dishObj = dishArray.getJSONObject(i);
+            tempDish.setId(Integer.parseInt(dishObj.getString("id")));
+            tempDish.setName(dishObj.getString("name"));
+            tempDish.setPrice(Double.parseDouble(dishObj.getString("price")));
+            tempDish.setDescription(dishObj.getString("description"));
+            tempDish.setImageServer(dishObj.getString("image"));
+            tempDish.setCreatedOn(new Date(dishObj.getString("created_on")));
+            tempDish.setUpdatedOn(new Date(dishObj.getString("updated_on")));
+            resultList.add(tempDish);
+        }
+        return resultList;
+    }
+    public List<Category> parseCategorysResp(String categoryRespStr) throws JSONException{
+        List<Category> resultList = null; 
+        //JSONObject jResp = Util.parseJson(responseStr);
+        JSONObject dishs = new JSONObject(categoryRespStr);
+        resultList = new ArrayList<Category>();
+        int i;
+        Category tempCategory = null;
+        JSONArray dishArray = dishs.getJSONArray("categories");
+        for (i = 0;i < dishArray.length();i ++){
+            tempCategory = new Category();
+            JSONObject dishObj = dishArray.getJSONObject(i);
+            tempCategory.setId(Integer.parseInt(dishObj.getString("id")));
+            tempCategory.setName(dishObj.getString("name"));
+            tempCategory.setDescription(dishObj.getString("description"));
+            tempCategory.setSortOrder(Integer.parseInt(dishObj.getString("sort_order")));
+            resultList.add(tempCategory);
+        }
+        return resultList;
+    }
+
+    public List<DishCategory> parseDishCategorysResp(String dishCategoryRespStr) throws JSONException{
+        List<DishCategory> resultList = null;
+        //JSONObject jResp = Util.parseJson(responseStr);
+        JSONObject dishs = new JSONObject(dishCategoryRespStr);
+        resultList = new ArrayList<DishCategory>();
+        int i;
+        DishCategory tempDishCategory = null;
+        JSONArray dishArray = dishs.getJSONArray("dish_categories");
+        for (i = 0;i < dishArray.length();i ++){
+            tempDishCategory = new DishCategory();
+            JSONObject dishObj = dishArray.getJSONObject(i);
+            tempDishCategory.setDishId(Integer.parseInt(dishObj.getString("dish_id")));
+            tempDishCategory.setCategoryId(Integer.parseInt(dishObj.getString("category_id")));
+            resultList.add(tempDishCategory);
+        }
+        return resultList;
+    }
+
+    public List<Order> parseOrdersResp(String orderRespStr) throws JSONException{
+        List<Order> resultList = null;
+        //JSONObject jResp = Util.parseJson(responseStr);
+        JSONObject dishs = new JSONObject(orderRespStr);
+        resultList = new ArrayList<Order>();
+        int i;
+        Order tempOrder = null;
+        JSONArray dishArray = dishs.getJSONArray("orders");
+        for (i = 0;i < dishArray.length();i ++){
+            tempOrder = new Order();
+            JSONObject dishObj = dishArray.getJSONObject(i);
+            tempOrder.setId(Integer.parseInt(dishObj.getString("_id")));
+            tempOrder.setTableId(Integer.parseInt(dishObj.getString("table_id")));
+            tempOrder.setStatus(Integer.parseInt(dishObj.getString("status")));
+            tempOrder.setOrderTotal(Double.parseDouble(dishObj.getString("order_total")));
+            tempOrder.setPaidOn(new Date(dishObj.getString("paid_on")));
+            tempOrder.setCreatedOn(new Date(dishObj.getString("created_on")));
+            resultList.add(tempOrder);
+        }
+        return resultList;
+    }
+
+    public List<OrderDetail> parseOrderDetailsResp(String orderDetailRespStr) throws JSONException{
+        List<OrderDetail> resultList = null;
+        //JSONObject jResp = Util.parseJson(responseStr);
+        JSONObject dishs = new JSONObject(orderDetailRespStr);
+        resultList = new ArrayList<OrderDetail>();
+        int i;
+        OrderDetail tempOrderDetail = null;
+        JSONArray dishArray = dishs.getJSONArray("order_details");
+        for (i = 0;i < dishArray.length();i ++){
+            tempOrderDetail = new OrderDetail();
+            JSONObject dishObj = dishArray.getJSONObject(i);
+            tempOrderDetail.setId(Integer.parseInt(dishObj.getString("_id")));
+            tempOrderDetail.setOrderId(Integer.parseInt(dishObj.getString("order_id")));
+            tempOrderDetail.setOrderDetailId(Integer.parseInt(dishObj.getString("order_id")));
+            tempOrderDetail.setDishId(Integer.parseInt(dishObj.getString("dish_id")));
+            tempOrderDetail.setNumber(Integer.parseInt(dishObj.getString("number")));
+            resultList.add(tempOrderDetail);
+        }
+        return resultList;
+    }
+    
+
+    public List<DiningTable> parseDiningTablesResp(String diningTablesRespStr) throws JSONException{
+        List<DiningTable> resultList = null;
+        //JSONObject jResp = Util.parseJson(responseStr);
+        JSONObject dishs = new JSONObject(diningTablesRespStr);
+        resultList = new ArrayList<DiningTable>();
+        int i;
+        DiningTable tempDiningTable = null;
+        JSONArray dishArray = dishs.getJSONArray("dining_tables");
+        for (i = 0;i < dishArray.length();i ++){
+            tempDiningTable = new DiningTable();
+            JSONObject dishObj = dishArray.getJSONObject(i);
+            tempDiningTable.setId(Integer.parseInt(dishObj.getString("_id")));
+            tempDiningTable.setName(dishObj.getString("name"));
+            tempDiningTable.setMaxPeople(Integer.parseInt(dishObj.getString("max_people")));
+            int free = Integer.parseInt(dishObj.getString("is_free"));            
+            tempDiningTable.setFree(free == 0? true : false);
+            resultList.add(tempDiningTable);
+        }
+        return resultList;
+    }
+    
+
+    public List<Config> parseConfigsResp(String configsRespStr) throws JSONException{
+        List<Config> resultList = null;
+        //JSONObject jResp = Util.parseJson(responseStr);
+        JSONObject dishs = new JSONObject(configsRespStr);
+        resultList = new ArrayList<Config>();
+        int i;
+        Config tempConfig = null;
+        JSONArray dishArray = dishs.getJSONArray("configs");
+        for (i = 0;i < dishArray.length();i ++){
+            tempConfig = new Config();
+            JSONObject dishObj = dishArray.getJSONObject(i);
+            tempConfig.setId(Integer.parseInt(dishObj.getString("_id")));
+            tempConfig.setName(dishObj.getString("name"));
+            tempConfig.setValue(dishObj.getString("value"));
+            tempConfig.setDescription(dishObj.getString("description"));
+            resultList.add(tempConfig);
+        }
+        return resultList;
+    }
+}
---
+++ src/com/androidodc/eorder/engine/RequestHelper.java
@@ -0,0 +1,134 @@
+package com.androidodc.eorder.engine;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import android.os.Bundle;
+import android.os.Environment;
+
+public class RequestHelper {
+	public String doPost(String url, Bundle params) throws Exception{
+        HttpPost httpRequest = new HttpPost(url);  
+        List paramsList = new ArrayList();
+        BasicNameValuePair paramPair;
+        for (String key : params.keySet()) {
+            paramPair = new BasicNameValuePair(key, params.getString(key));
+            paramsList.add(paramPair);
+        }
+        try{ 
+            httpRequest.setEntity(new UrlEncodedFormEntity(paramsList, HTTP.UTF_8));
+            HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest); 
+            if(httpResponse.getStatusLine().getStatusCode() == 200) { 
+                String strResult = EntityUtils.toString(httpResponse.getEntity());
+                return strResult;
+            }else{      
+                return "Http response access status error!";
+            }   
+        }catch (Exception e){ 
+            throw e;
+        } 
+    }
+    public String doGet(String url, Bundle params) throws Exception{
+        if(params != null) {
+            StringBuilder sb = new StringBuilder();
+            boolean first = true;
+            for (String key : params.keySet()) {
+                if (first){
+                    first = false; 
+                }else{
+                    sb.append("&");
+                }
+                sb.append(URLEncoder.encode(key) + "=" +
+                          URLEncoder.encode(params.getString(key)));
+            }
+            url = url + "?" + sb.toString();
+        }
+
+        HttpGet httpRequest = new HttpGet(url);
+        try{
+            HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest); 
+            if(httpResponse.getStatusLine().getStatusCode() == 200){  
+                String strResult = EntityUtils.toString(httpResponse.getEntity());  
+                return strResult;   
+            }else{   
+                return "Http response access status error!";   
+            }
+        }catch (Exception e){
+            throw e;
+        }
+    }
+    public boolean getFileFromServer(String path, Bundle params, OutputStream fos){
+         byte[] bf = new byte[1024];
+         int current=0;
+         try {
+             if(params != null) {
+                 StringBuilder sb = new StringBuilder();
+                 boolean first = true;
+                 for (String key : params.keySet()) {
+                     if (first){
+                         first = false; 
+                     }else{
+                         sb.append("&");
+                     }
+                     sb.append(URLEncoder.encode(key) + "=" +
+                               URLEncoder.encode(params.getString(key)));
+                 }
+                 path = path + "?" + sb.toString();
+             }
+             //创建一个URL对象
+             URL url = new URL(path);//spec:资源连接地址
+             HttpURLConnection connect = (HttpURLConnection)url.openConnection();
+             //从服务端下载Http资源，设置读取权限
+             connect.setDoInput(true);
+             // //上传资源到服务端，设置写入权限
+             // connect.setDoOutput(true);
+             //设置连接服务器超时时间
+             connect.setConnectTimeout(5 * 1000);
+             //设置从服务器读取数据超时时间
+             connect.setReadTimeout(30 * 1000);
+             //获得网络连接状态码
+             int code = connect.getResponseCode();
+             //判断是否连接成功（HttpURLConnection.HTTP_OK==200,0-200都属正常）
+             if(code == HttpURLConnection.HTTP_OK){
+             // //打开连接获取资源（不写也没关系，调用getInputStream()默认会打开连接
+              // connect.connect();
+                 InputStream is = connect.getInputStream();
+                 BufferedInputStream bis = new BufferedInputStream(is);
+                 while((current = bis.read(bf)) != -1){
+                     fos.write(bf, 0, current);
+                 }
+                 bis.close();
+                 fos.close();
+                 //断开连接
+                 connect.disconnect();
+             }
+             return true;
+         } catch (MalformedURLException e) {
+             // TODO Auto-generated catch block
+             e.printStackTrace();
+             return false;
+         } catch (IOException e) {
+             // TODO Auto-generated catch block
+             e.printStackTrace();
+             return false;
+         }
+    }
+}
