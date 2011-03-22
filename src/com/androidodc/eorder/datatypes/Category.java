ae97c20598d968658e0a769917211cdd989293d4
keer ye <Jialei.Ye@myriadgroup.com>
2011-03-22 18:41:51 +0800

    eOrder: Modify Category.java and Dish.java, delete DishCategory.java
    Description: Move class DishCategory into Category.java file, and
    change Dish several fields's name or type.

--- src/com/androidodc/eorder/datatypes/Category.java
+++ src/com/androidodc/eorder/datatypes/Category.java
@@ -1,19 +1,22 @@
 package com.androidodc.eorder.datatypes;
 
-public class Category {
+import java.io.Serializable;
 
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 3948922015270201885L;
     private int mId;
-    private int mSortOrder;
     private int mCategoryId;
     private String mName;
     private String mDescription;
-
-    public void setId(int id) {
-        mId = id;
-    }
+    private int mSortOrder;
 
     public int getId() {
         return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
     }
 
     public int getCategoryId() {
@@ -40,11 +43,32 @@ public class Category {
         mDescription = description;
     }
 
-    public int getSortOrder() {
+    public Integer getSortOrder() {
         return mSortOrder;
     }
 
     public void setSortOrder(Integer sortOrder) {
         mSortOrder = sortOrder;
+    }
+
+    public class DishCategory {
+        private int mCategoryId;
+        private int mDishId;
+
+        public int getCategoryId() {
+            return mCategoryId;
+        }
+
+        public void setCategoryId(int categoryId) {
+            mCategoryId = categoryId;
+        }
+
+        public int getDishId() {
+            return mDishId;
+        }
+
+        public void setDishId(int dishId) {
+            mDishId = dishId;
+        }
     }
 }
