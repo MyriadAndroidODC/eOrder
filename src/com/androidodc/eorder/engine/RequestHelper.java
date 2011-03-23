0e9e35a268c3493b8e14a3e8c018e2bbd0a44244
Carl Li <carlli.testman@gmail.com>
2011-03-23 15:40:58 +0800

    Modify format with request helper.

--- src/com/androidodc/eorder/engine/RequestHelper.java
+++ src/com/androidodc/eorder/engine/RequestHelper.java
@@ -16,43 +16,28 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
+
+import com.androidodc.eorder.utils.LogUtils;
+
 import android.os.Bundle;
 
 public class RequestHelper {
-	public String doPost(String url, Bundle params) throws Exception {
-        HttpPost httpRequest = new HttpPost(url);  
-        List paramsList = new ArrayList();
-        BasicNameValuePair paramPair;
-        
-        for (String key : params.keySet()) {
-            paramPair = new BasicNameValuePair(key, params.getString(key));
-            paramsList.add(paramPair);
-        }
-        
-        try { 
-            httpRequest.setEntity(new UrlEncodedFormEntity(paramsList, HTTP.UTF_8));
-            HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest); 
-            
-            if (httpResponse.getStatusLine().getStatusCode() == 200) { 
-                String strResult = EntityUtils.toString(httpResponse.getEntity());
-                return strResult;
-            } else {      
-                return "Http response access status error!";
-            }   
-        } catch (Exception e) { 
-            throw e;
-        } 
-    }
-	
-    public String doGet(String url, Bundle params) throws Exception {
+    private static final int SUCCESS_STATUS = 200;
+    private static final int FILE_END = -1;
+    private static final int CONNECTION_TIME_OUT = 2000;
+    private static final int SOCKET_TIME_OUT = 3000;
+    
+    public String doGet(String url, Bundle params) {
         if (params != null) {
             StringBuilder sb = new StringBuilder();
             boolean first = true;
             
             for (String key : params.keySet()) {
-                
                 if (first) {
                     first = false; 
                 } else {
@@ -66,20 +51,59 @@ public class RequestHelper {
         }
 
         HttpGet httpRequest = new HttpGet(url);
+        HttpParams httpParameters = new BasicHttpParams(); 
+        HttpConnectionParams.setConnectionTimeout(httpParameters, CONNECTION_TIME_OUT); 
+        HttpConnectionParams.setSoTimeout(httpParameters, SOCKET_TIME_OUT); 
+      
         try {
-            HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest);
+            HttpResponse httpResponse = new DefaultHttpClient(httpParameters).execute(httpRequest);
             
-            if (httpResponse.getStatusLine().getStatusCode() == 200) {  
+            if (httpResponse.getStatusLine().getStatusCode() == SUCCESS_STATUS) {  
                 String strResult = EntityUtils.toString(httpResponse.getEntity());  
                 return strResult;                 
-            } else {   
-                return "Http response access status error!";   
+            } else {
+                LogUtils.logD("Http response error when do get operation!");
+                return null;
             }
         } catch (Exception e) {
-            throw e;
+            LogUtils.logD(e.getMessage());
+            return null;
         }
     }
-    
+        
+    public String doPost(String url, Bundle params) {
+        HttpPost httpRequest = new HttpPost(url);  
+        List paramsList = new ArrayList();
+        BasicNameValuePair paramPair;
+        
+        if (params != null) {
+            for (String key : params.keySet()) {
+                paramPair = new BasicNameValuePair(key, params.getString(key));
+                paramsList.add(paramPair);
+            }
+        }
+
+        HttpParams httpParameters = new BasicHttpParams(); 
+        HttpConnectionParams.setConnectionTimeout(httpParameters, CONNECTION_TIME_OUT); 
+        HttpConnectionParams.setSoTimeout(httpParameters, SOCKET_TIME_OUT); 
+        
+        try { 
+            httpRequest.setEntity(new UrlEncodedFormEntity(paramsList, HTTP.UTF_8));
+            HttpResponse httpResponse = new DefaultHttpClient(httpParameters).execute(httpRequest); 
+            
+            if (httpResponse.getStatusLine().getStatusCode() == SUCCESS_STATUS) { 
+                String strResult = EntityUtils.toString(httpResponse.getEntity());
+                return strResult;
+            } else {
+                LogUtils.logD("Http response error when do post operation!");
+                return null;
+            }
+        } catch (Exception e) {
+            LogUtils.logD(e.getMessage());
+            return null;
+        } 
+    }
+
     public boolean getFileFromServer(String path, Bundle params, OutputStream fos) {
          byte[] bf = new byte[1024];
          int current = 0;
@@ -112,7 +136,7 @@ public class RequestHelper {
              if (code == HttpURLConnection.HTTP_OK) {
                  InputStream is = connect.getInputStream();
                  BufferedInputStream bis = new BufferedInputStream(is);
-                 while((current = bis.read(bf)) != -1){
+                 while((current = bis.read(bf)) != FILE_END){
                      fos.write(bf, 0, current);
                  }
                  
@@ -122,12 +146,10 @@ public class RequestHelper {
              }
              return true;
          } catch (MalformedURLException e) {
-             // TODO Auto-generated catch block
-             e.printStackTrace();
+             LogUtils.logD(e.getMessage());
              return false;
          } catch (IOException e) {
-             // TODO Auto-generated catch block
-             e.printStackTrace();
+             LogUtils.logD(e.getMessage());
              return false;
          }
     }
