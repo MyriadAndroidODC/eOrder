dee89740f407b369d343341648ca2c0722f76bec
Edward Gu <edward.gu@myriadgroup.com>
2011-03-24 18:11:44 +0800

    update

--- src/com/androidodc/eorder/MainApp.java
+++ src/com/androidodc/eorder/MainApp.java
@@ -3,8 +3,6 @@ package com.androidodc.eorder;
 import android.app.Application;
 import android.os.StrictMode;
 
-import com.androidodc.eorder.utils.LogUtils;
-
 public class MainApp extends Application {
 
     public void onCreate() {
