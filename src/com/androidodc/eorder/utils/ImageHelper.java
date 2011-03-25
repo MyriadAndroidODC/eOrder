7e622dc426453cb9d7367b3b3a14462d3dda1650
Edward Gu <edward.gu@myriadgroup.com>
2011-03-25 10:51:55 +0800

    eOrder: imagehelper update
    implemented api to make it work.

--- src/com/androidodc/eorder/utils/ImageHelper.java
+++ src/com/androidodc/eorder/utils/ImageHelper.java
@@ -1,10 +1,39 @@
 package com.androidodc.eorder.utils;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+/*
+ * Help to get images (in Bitmap), and manage image cache.
+ * FIX ME: load image files in other threads.
+ */
 
 public class ImageHelper {
 
+    private static ImageCache mCache = ImageCache.getInstance();
+    private static boolean mEnableCache = true;
+
     public static Bitmap getImage(String imageLocation) {
-        return Bitmap.createBitmap(120, 120, Bitmap.Config.ARGB_4444);
+        Bitmap _image;
+        if (mEnableCache) {
+            _image = mCache.get(imageLocation);
+            if (_image == null) {
+                _image = BitmapFactory.decodeFile(imageLocation);
+                if (_image != null) {
+                    mCache.put(imageLocation, _image);
+                }
+            }
+        } else {
+            _image = BitmapFactory.decodeFile(imageLocation);
+        }
+        return _image;
+    }
+
+    public static void enableCache() {
+        mEnableCache = true;
+    }
+
+    public static void disableCache() {
+        mEnableCache = false;
     }
 }
